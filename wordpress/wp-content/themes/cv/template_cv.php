76c7c858c0fa3c868c5a242b169e2ae50f728dd2
karina BRANTE <karina.brante@gmail.com>
2017-04-26 12:05:03 +0200

    category and post

--- wordpress/wp-content/themes/cv/template_cv.php
+++ wordpress/wp-content/themes/cv/template_cv.php
@@ -1,12 +1,42 @@
 <?php
     /* Template Name: cv */
-    get_header();
 
  ?>
-
-
 <?php
-    get_sidebar();
-    get_footer();
+    $args = array('category_name' => "loisirs" );
+    $posts = get_posts($args);
+    ?>
+    <h2>loisirs</h2>
+    <?php
+       foreach ($posts as $post) {
+        setup_postdata($post);
+         ?>
 
- ?>
+         <p><a href= <?php the_permalink(); ?>><?php the_title(); ?></a></p>
+         <p><?php the_content(); ?></p>
+         <?php } ?>
+    <h2>competence</h2>
+    <?php
+    $args = array('category_name' => "competence" );
+    $posts = get_posts($args);
+        foreach ($posts as $post) {
+         setup_postdata($post);
+          ?>
+
+          <p><a href= <?php the_permalink(); ?>><?php the_title(); ?></a></p>
+          <p><?php the_content(); ?></p>
+
+<?php } ?>
+<?php
+    $args = array('category_name' => "formation" );
+    $posts = get_posts($args);
+    ?>
+    <h2>Formation</h2>
+    <?php
+       foreach ($posts as $post) {
+        setup_postdata($post);
+         ?>
+
+         <p><a href= <?php the_permalink(); ?>><?php the_title(); ?></a></p>
+         <p><?php the_content(); ?></p>
+         <?php } 
