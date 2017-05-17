ca116677034cdd52f4b6b3afad19b882bba50f73
karina BRANTE <karina.brante@gmail.com>
2017-05-17 10:10:42 +0200

    add bootstrap cdn

---
+++ wordpress/wp-content/themes/cv_2/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <ul id="social_nav">
+        <li>
+        <a href="#" target="blank"><i class="fa fa-facebook"></i></a>
+        </li>
+        <li>
+        <a href="#" target="blank"><i class="fa fa-twitter"></i></a>
+        </li>
+        <li>
+        <a href="#" target="blank"><i class="fa fa-google-plus-official"></i></a>
+        </li>
+        <li>
+        <a href="#" target="blank"><i class="fa fa-pinterest"></i></a>
+        </li>
+        <li>
+        <a href="#" target="blank"><i class="fa fa-youtube-play"></i></a>
+        </li>
+    </ul>
+    <p>Copyright © 2002-2016 SALWA S.L.</p>
+</footer>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
+</html>
--- wordpress/wp-content/themes/cv_2/index.php
+++ wordpress/wp-content/themes/cv_2/index.php
@@ -1,28 +1,43 @@
 <?php
 get_header();
-    $args = array('category_name' => "loisirs" );
-    $posts = get_posts($args);
-    ?>
-    <h2>loisirs</h2>
-    <?php
-        get_template_part( 'template-parts/content_loop_post');
+?>
+<body>
+    <div class="row">
+        <div class="col-md-12">
+            <?php
+            $args = array('category_name' => "loisirs" );
+            $posts = get_posts($args);
+            ?>
+            <h2 id="loisirs">loisirs</h2>
+            <?php
+            get_template_part( 'template-parts/content_loop_post');
 
-     ?>
+            ?>
 
-    <h2>competence</h2>
-    <?php
-        $args = array('category_name' => "competence" );
-        $posts = get_posts($args);
+            <h2 id="competence">competence</h2>
+            <?php
+            $args = array('category_name' => "competence" );
+            $posts = get_posts($args);
 
-     ?>
-     <?php
-     get_template_part( 'template-parts/content_loop_post');
+            ?>
+            <?php
+            get_template_part( 'template-parts/content_loop_post');
 
-      ?>
+            ?>
+            <?php
+            $args = array('category_name' => "formation" );
+            $posts = get_posts($args);
+            ?>
+            <h2 id="formation">Formation</h2>
+            <?php
+            get_template_part( 'template-parts/content_loop_post');
+            ?>
+        </div>
+    </div>
+</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
 <?php
-    $args = array('category_name' => "formation" );
-    $posts = get_posts($args);
-    ?>
-    <h2>Formation</h2>
-    <?php
-    get_template_part( 'template-parts/content_loop_post');
+get_footer();
+
+?>
+</body>
